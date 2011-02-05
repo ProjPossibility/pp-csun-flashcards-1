17bf7cfca8658d8988cfa19b98d290040d8b1ca8
itsbarakyo@gmail.com <itsbarakyo@gmail.com@671f0fa1-0aaa-5e3b-0bb5-c5971f7093c9>
2011-02-05 20:11:56 +0000

    Register PHP file and MYSQL include file for MYSQL info

---
+++ server/mysql.php
@@ -0,0 +1,9 @@
+<?php
+//this file is used to include information for the mysql info
+//so that users wont have to keep modifying the password
+
+//mysql info
+$mysql_user = 'victor';
+$mysql_pass = 'Dr5q33yVSumme4Ry';
+
+?>
