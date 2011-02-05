b50d54747fdff30fee6c952f815ab45c3bb37c5f
cederstrom.c@gmail.com <cederstrom.c@gmail.com@671f0fa1-0aaa-5e3b-0bb5-c5971f7093c9>
2011-02-05 21:25:59 +0000

    Master page

--- web/_vti_cnf/view_flashcard.php
+++ web/_vti_cnf/view_flashcard.php
@@ -1,14 +1,15 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|CERBERUS\\Chris
 vti_modifiedby:SR|CERBERUS\\Chris
-vti_timelastmodified:TR|05 Feb 2011 20:43:58 -0000
-vti_timecreated:TR|05 Feb 2011 20:03:22 -0000
+vti_timelastmodified:TR|05 Feb 2011 20:40:49 -0000
+vti_timecreated:TR|05 Feb 2011 20:18:33 -0000
+vti_title:SR|View Flashcard
 vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|view_flashcard.php create_flashcard.html master.dwt index.html login.html
-vti_nexttolasttimemodified:TR|05 Feb 2011 20:31:21 -0000
-vti_cacheddtm:TX|05 Feb 2011 20:43:58 -0000
-vti_filesize:IR|1637
-vti_cachedtitle:SR|Create Flashcard
+vti_backlinkinfo:VX|create_flashcard.html view_flashcard.php master.dwt index.html login.html
+vti_nexttolasttimemodified:TR|05 Feb 2011 20:31:23 -0000
+vti_cacheddtm:TX|05 Feb 2011 20:40:49 -0000
+vti_filesize:IR|827
+vti_cachedtitle:SR|View Flashcard
 vti_cachedbodystyle:SR|<body>
 vti_cachedlinkinfo:VX|G|master.dwt H|index.html H|create_flashcard.html H|view_flashcard.php H|login.html
 vti_cachedsvcrellinks:VX|FGUS|master.dwt FHUS|index.html FHUS|create_flashcard.html FHUS|view_flashcard.php FHUS|login.html
@@ -18,4 +19,3 @@ vti_cachedhastheme:BR|false
 vti_cachedhasborder:BR|false
 vti_metatags:VR|HTTP-EQUIV=Content-Type text/html;\\ charset=utf-8
 vti_charset:SR|utf-8
-vti_title:SR|Create Flashcard
